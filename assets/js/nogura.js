878e4ace48999b4e8bc5a8111b13c5943e745ca8
Natalia Sanchez <nataliasmdev@gmail.com>
2020-04-04 21:23:44 +0200

    Muchos avances

--- assets/js/nogura.js
+++ assets/js/nogura.js
@@ -8,23 +8,7 @@ $(function() {
 function checkWindowScroll(onLoad = false) {
 	if ($(window).scrollTop() != 0) {
     	$("body").addClass("scrolled");
-    	if (onLoad) {
-			$(".mainMenu .mainLogo").hide();
-			$(".mainMenu .alterLogo").show();
-    	} else {
-    		$(".mainMenu .mainLogo").fadeOut(100, function() {
-				$(".mainMenu .alterLogo").fadeIn();
-			});
-    	}
     } else {
     	$("body").removeClass("scrolled");
-    	if (onLoad) {
-			$(".mainMenu .mainLogo").show();
-			$(".mainMenu .alterLogo").hide();
-    	} else {
-			$(".mainMenu .alterLogo").fadeOut(150, function() {
-				$(".mainMenu .mainLogo").fadeIn();
-			});
-    	}
     }
 }
